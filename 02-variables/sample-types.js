338a5b3c030fca964b2bc729687f201a57793cdb
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-08-29 17:58:38 -0400

    Sample Types

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,9 @@
+var found = true;
+var grande = 88.6;
+var firstName = "Alexandre";
+var lastName = 'Macedo';
+console.log(found);
+console.log("The grade is: " + grande);
+console.log("Hi " + firstName + " " + lastName);
+//use template string
+console.log("Hi ".concat(firstName, " ").concat(lastName));
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,12 @@
+let found: boolean = true;
+let grande: number = 88.6;
+let firstName: string = "Alexandre";
+let lastName: string = 'Macedo';
+
+console.log(found);
+console.log("The grade is: " + grande);
+console.log("Hi " + firstName + " " + lastName);
+
+//use template string
+
+console.log(`Hi ${firstName} ${lastName}`)
